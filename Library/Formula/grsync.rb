e5e924d27ea40ab30694d713bc929a815a647796
Mark Histed <mhisted@gmail.com>
2014-08-20 17:36:57 -0400

    add gettext as an explicit dependency
    Signed-off-by: Mark Histed <mhisted@gmail.com>

--- Library/Formula/grsync.rb
+++ Library/Formula/grsync.rb
@@ -6,16 +6,16 @@ class Grsync < Formula
   sha256 '5e74819a9188a5f722b8a692d8df0bc011c3ff1f1e8e4bbd8e5989b76e46c370'
 
   depends_on 'pkg-config' => :build
-  depends_on 'gtk+'
   depends_on 'intltool' => :build
+  depends_on 'gettext'
+  depends_on 'gtk+'
+
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-unity",
                           "--prefix=#{prefix}"
 
-    ENV['LDFLAGS'] = '-L/usr/local/opt/gettext/lib'
-    ENV['CPPFLAGS'] = '-I/usr/local/opt/gettext/include'
     system "make", "install"
   end
 
